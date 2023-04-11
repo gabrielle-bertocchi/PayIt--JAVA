1b5c4062ae6a4e9bbb0c435035e4fee038350090
Gabrielle Bertocchi Sales <99894790+gabrielle-bertocchi@users.noreply.github.com>
2023-04-11 18:07:36 +0100

    add condiction

--- src/Jogo.java
+++ src/Jogo.java
@@ -7,13 +7,23 @@ public class Jogo {
         System.out.println("Choose a direction (w, d, s, a): ");
 
         Scanner scan = new Scanner(System.in);
-        String opcao = scan.nextLine();
-        System.out.println("You wrote this: " + opcao);
-        System.out.println("You wrote this: " + opcao);
+        String opcao;
 
-        
+        do{
+            opcao = scan.nextLine();
 
-        
+            if(opcao.equals("w")){
+                System.out.println("Wall! You hit it in the head...");
+            }else if(opcao.equals("d")) {
+                System.out.println("Wild wolves! You've been devoured...");
+            }else if (opcao.equals("s")) {
+                System.out.println("Careful! Monster! You were crushed...");
+            }else if(opcao.equals("a")) {
+                System.out.println("You found the way out! Congratulations...");
+            }
+
+
+        }while(!opcao.equals("a"));
     }
 
 }
