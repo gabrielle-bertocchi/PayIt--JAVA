851b8753152cdaa7506080c01f1d1f3490d754b9
Gabrielle Bertocchi Sales <99894790+gabrielle-bertocchi@users.noreply.github.com>
2023-04-11 17:57:49 +0100

    .

---
+++ src/Jogo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Jogo {
+    public static void main(String[] args) {
+
+        System.out.println("You are in a house in the middle of the forest");
+        System.out.println("Choose a direction (w, d, s, a): ");
+
+        Scanner scan = new Scanner(System.in);
+        String opcao = scan.nextLine();
+        System.out.println("You wrote this: " + opcao);
+        System.out.println("You wrote this: " + opcao);
+
+        
+
+        
+    }
+
+}
